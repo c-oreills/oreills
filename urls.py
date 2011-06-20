396e9dd182395e04da172324af7e1ca1b60df61f
Christy O'Reilly <christyor73@gmail.com>
2011-06-20 21:20:37 +0100

    Add user login to urls

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.auth.views import login
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,6 +15,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
 
     (r'^subterreader/$', 'subterreader.views.manage'),
     (r'^subterreader/read/$', 'subterreader.views.read'),
