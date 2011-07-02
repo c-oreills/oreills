f9fc17682dd5b6b0ee4e78f2ecd3fe1d55fa74dd
Christy O'Reilly <christyor73@gmail.com>
2011-07-02 17:50:03 +0100

    Make subterreader template files modular

--- settings.py
+++ settings.py
@@ -100,6 +100,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(BASE_PATH, 'templates'),
+    os.path.join(BASE_PATH, 'subterreader', 'templates'),
 )
 
 INSTALLED_APPS = (
