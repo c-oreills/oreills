bf9758f8bff196ae1fbad51f06691329d0e3eec4
Christy O'Reilly <christyor73@gmail.com>
2011-07-06 20:26:03 +0100

    Add shpaml support

--- settings.py
+++ settings.py
@@ -80,10 +80,17 @@ STATICFILES_FINDERS = (
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
+    'shpaml_loader.filesystem',
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
 )
+
+# Use cacheing of shpaml in production
+if not DEBUG:
+    TEMPLATE_LOADERS = (
+        ('django.template.loaders.cached.Loader', TEMPLATE_LOADERS),
+    )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
