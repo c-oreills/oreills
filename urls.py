16b379b1900f36489646c2cab19034d41b397459
Christy O'Reilly <christyor73@gmail.com>
2011-08-23 21:44:06 +0100

    Add logout page

--- urls.py
+++ urls.py
@@ -22,6 +22,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
 
     (r'^subterreader/', include('subterreader.urls')),
 )
