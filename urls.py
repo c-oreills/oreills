4069d860c4f6ce00b1f25cef1f7a9cc1282a27b7
Christy O'Reilly <christyor73@gmail.com>
2011-09-03 18:28:48 +0100

    Tidy urls, add cv

--- urls.py
+++ urls.py
@@ -1,25 +1,21 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.auth.views import login
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.generic.simple import direct_to_template
+from django.views.generic.simple import direct_to_template, redirect_to
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', direct_to_template, dict(template='oreills/about.html.haml')),
-    #url(r'^about/$', direct_to_template, dict(template='oreills/about.html.haml')),
     url(r'^contact/$', direct_to_template, dict(template='oreills/contact.html.haml')),
     url(r'^projects/$', direct_to_template, dict(template='oreills/projects.html.haml')),
     url(r'^projects/av$', direct_to_template, dict(template='av/av.html.haml')),
-    # url(r'^oreills/', include('oreills.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^cv$', redirect_to, dict(url='%smisc/christy-oreilly-curriculum-vitae.pdf' % (settings.STATIC_URL))),
 
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
